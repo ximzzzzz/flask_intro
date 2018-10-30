940a02ae6b94242b8ec2354d87bd77c58a30ad40
ethan <ximzzzzz@gmail.com>
2018-10-30 06:53:53 +0000

    opgg..

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 import random
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -51,3 +53,44 @@ def lunch():
 def lotto():
     lottonumber = random.sample(range(46),6)
     return render_template('lotto.html', lottonumber = lottonumber)
+    
+@app.route('/naver')
+def naver():
+    return render_template('naver.html')
+    
+    
+@app.route('/google')
+def google():
+    return render_template('google.html')
+    
+@app.route('/hello')
+def halo():
+    return render_template('halo.html')
+    
+@app.route("/hi")
+def hi():
+    user_name = request.args.get('name')
+    return render_template('hi.html', user_name=user_name)
+    
+@app.route('/hair_match')
+def hair():
+    return render_template('hair.html')
+    
+@app.route('/star')
+def star():
+    starname = request.args.get('starname')
+    return render_template('star.html',starname=starname)
+  
+@app.route('/summoner')
+def summoner():
+    return render_template('summoner.html')
+    
+@app.route('/opgg')
+def opgg():
+    summon=request.args.get('summoner')
+    url='http://www.op.gg/summoner/userName='+summon
+    html = requests.get(url).text
+    soup = BeautifulSoup(html,'html.parser')
+    win = soup.select_one('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins').text
+    lose = soup.select_one('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.losses').text
+    return render_template('opgg.html', win=win, lose=lose,summon=summon)
