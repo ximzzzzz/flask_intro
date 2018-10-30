9b1193ea637695bb83ce5c783cf5de8aff89d3e8
ethan <ximzzzzz@gmail.com>
2018-10-30 02:31:59 +0000

    flask url setting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -21,7 +21,18 @@ def html_line():
     <li>2번</li></ul>
     """
     
+@app.route("/html_file")
+def html_file():
+    return render_template("file.html")
     
+@app.route("/hello_name/<string:name>")
+def hello_name(name):
+    return render_template("hello.html", people_name = name)
+    
+@app.route("/cube/<int:num>")
+def cube(num):
+    result = num**3
+    return render_template("cube.html", num=num, cube_num=result)
     
     
     
