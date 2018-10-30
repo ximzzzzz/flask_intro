67a1d2dd6fe8b4deb50e77dcb097cebe932201f8
ethan <ximzzzzz@gmail.com>
2018-10-30 04:21:34 +0000

    add lotto

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import random
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -34,8 +36,18 @@ def cube(num):
     result = num**3
     return render_template("cube.html", num=num, cube_num=result)
     
+@app.route("/lunch")
+def lunch():
+    list =['20층', '김카', '진가와']
+    dict = {'20층':'http://newsplus.chosun.com/site/data/img_dir/2010/04/12/2010041201333_0.jpg',
+    '김카': 'https://igx.4sqi.net/img/general/600x600/25224626_w2nCYA2f3tJthJ0Sbcz9fpke3yLetCy6kr-O9-AXkco.jpg',
+    '진가와' : 'https://t1.daumcdn.net/cfile/tistory/990AC73D5B5DD50E06'}
     
+    chosen =random.choice(list)
+    pick = dict[chosen]
+    return render_template("lunch.html", chosen=pick)
     
-@app.route("/donguni")
-def donguni():
-    return "<h1>보이냐 개씨발새끼야?!</h1>"
+@app.route('/lotto')
+def lotto():
+    lottonumber = random.sample(range(46),6)
+    return render_template('lotto.html', lottonumber = lottonumber)
